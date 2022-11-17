6808114d822d67a335f9cf0eaaf992561ec11daf
Sir Qasim <m.qasim077@gmail.com>
2022-11-17 15:45:20 +0500

    Add files via upload

---
+++ day4/Home_page_update_project/mysite/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import  views
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+    path('', views.home),
+
+]
